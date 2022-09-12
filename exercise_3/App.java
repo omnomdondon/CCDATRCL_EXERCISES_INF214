55e06a51b76d116338fa36306e369f533d601c7b
Brandon Kenneth Cinco <112753462+omnomdondon@users.noreply.github.com>
2022-09-12 22:56:38 +0800

    ADDED FILES

---
+++ exercise_3/App.java
@@ -0,0 +1,33 @@
+// Array implementation of Stack
+public class App {
+    public static void main(String[] args) {
+
+
+        // Create new Stack Object
+        Stack tshirt_stack = new Stack(5);
+
+        // Insert new elements into the stack
+        tshirt_stack.push("Black Shirt");
+        tshirt_stack.push("White Shirt");
+        tshirt_stack.push("Grey Shirt");
+        tshirt_stack.push("Blue Shirt");
+        tshirt_stack.push("Red Shirt");
+
+        //Show the size of the stack
+        System.out.println("The size of the stack is: " + tshirt_stack.size());
+
+        // Show top element in the stack
+        System.out.println("This top of the stack is: " + tshirt_stack.peek());
+
+        // Remove top element in the stack
+        tshirt_stack.pop();
+
+        System.out.println("After pop");
+
+        // Show all elements in Stack
+        tshirt_stack.printStack();
+
+        // Show top element in the stack
+        System.out.println("The top of the stack is: " + tshirt_stack.peek());
+    }
+}
